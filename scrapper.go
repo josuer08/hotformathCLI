775386f06475c47dcdf27655039f820b89e26e37
josuer08 <josuer08@gmail.com>
2022-08-09 23:25:36 -0400

    another chunk lol, it is functional already

--- scrapper.go
+++ scrapper.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/rocketlaunchr/google-search"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/rocketlaunchr/google-search"
 )
 
 func main() {
@@ -13,9 +15,44 @@ func main() {
     CountryCode: "us",
     LanguageCode: "en",
     }
-    q:="Mr Robot"
+
+
+
     common:=" -inurl:(jsp|pl|php|html|aspx|htm|cf|shtml) -inurl:(index_of|listen77|mp3raid|mp3toss|mp3drug|index_of|wallywashis) intitle:\"index.of./\" "
-    returnLinks, err := googlesearch.Search(ctx, q, opts)
+
+    VIDEO:=" (avi|mkv|mov|mp4|mpg|wmv)"
+    AUDIO:=" (ac3|flac|m4a|mp3|ogg|wav|wma)"
+    EBOOK:=" (CBZ|CBR|CHM|DOC|DOCX|EPUB|MOBI|ODT|PDF|RTF|txt)"
+    PICTURES:=" (bmp|gif|jpg|png|psd|tif|tiff)"
+    SOFTWARE:=" (apk|exe|iso|rar|tar|zip)"
+    COMPRESSED:=" (7z|bz2|gz|iso|rar|zip)"
+
+    // implement getModifier()
+
+    modifier:=""
+    switch os.Args[1] {
+        case "-v":
+            modifier = VIDEO
+        case "-a":
+            modifier = AUDIO
+        case "-e":
+            modifier = EBOOK
+        case "-p":
+            modifier = PICTURES
+        case "-s":
+            modifier = SOFTWARE
+        case "-c":
+            modifier = COMPRESSED
+        default:
+            fmt.Println("Usage: scrapper -[flag] \"[your search]\"")
+            fmt.Println("flags = [a]udio [c]ompressed [e]book [p]ictures [s]oftware [v]ideo")
+            os.Exit(0)
+    }
+
+    query := "intext:\"" + os.Args[2] + "\"" + common + modifier
+
+
+    returnLinks, err := googlesearch.Search(ctx, query, opts)
     if err != nil {
         fmt.Println("Something went wrong: ", err)
     return
@@ -28,5 +65,4 @@ func main() {
         fmt.Println("*", result.URL)
 
     }
-    //fmt.Println(googlesearch.Search(ctx, "cars for sale in Toronto, Canada"))
 }
